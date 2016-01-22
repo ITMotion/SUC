7955c0d7b618e5575a3af29c90f750b9bd450021
RockLogin <wayne6@hotmail.com>
2016-01-22 13:07:03 -0500

    login

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>SUC</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+	<div class="container" id="content">
+		<div id="contenedor">
+			<div id="titulo">
+				<div class="row">
+					<div class="col-md-12" align="center">
+						<h1>Bienvenido<br><small>Sistema Único de Calificaciones</small></h1>	
+					</div>
+				</div>
+			</div>
+			<div class="row" id="login">
+				<div class="col-md-8" align="center">
+					<form class="form">
+				  		<div class="form-group">
+				    		<label class="sr-only" for="inputUser">Usuario</label><br>
+				    		<input type="text" class="form-control" id="inputUser" placeholder="Usuario">
+				  		</div>
+				  		<div class="form-group">
+				   		 	<label class="sr-only" for="inputPassword">Contraseña</label>
+				    		<input type="password" class="form-control" id="inputPassword" placeholder="Contraseña">
+				  		</div>
+				  		<div class="checkbox">
+				   		 	<label>
+				      			<input type="checkbox"> <strong>Recordar Usuario</strong>
+				    	 	</label>
+				  		</div>
+				  		<br>
+			  			<button type="submit" class="btn btn-default">Entrar</button>
+			  			<br>
+			  			<br>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
