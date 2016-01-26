972249b140fe3778f1c2cf456529e8b4ac8946bb
bstsebastian <sebastian.paredes@hotmail.es>
2016-01-26 08:52:47 -0500

    crud alumnos
    agregar alumnos y mostrar.

---
+++ model/alum-ajax.js
@@ -0,0 +1,12 @@
+function getMateria(clave) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	var divGrupos = document.getElementById('Part2');
+    	divGrupos.innerHTML = "Cargando...";
+    	if(xhttp.readyState == 4) {
+    		divGrupos.innerHTML = xhttp.responseText;
+    	}
+    };
+    xhttp.open("GET", "AlumnosFrm2.php?codigo="+clave, true);
+    xhttp.send();
+}
---
+++ views/AlumnosFrm.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="en">
+	<title>SUC: Sistema Único de Calificaciones</title>
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<?php 
+		include_once("../model/DAOalum.php"); 
+		$db = new DAOalum();
+		$carreras = $db->getCarreras();
+		if (isset($_GET['success'])) {
+			if ($_GET['success']==2) {
+				echo "<script> alert('¡Este alumno ya existe!')</script>";
+			}
+		}
+		
+	?>
+</head>
+<body>
+	<?php include_once("menu.html") ?>
+	<div class="container">
+		<h1>Agregar alumno nuevo</h1>
+		<form action="../model/Alumnos-asignarAlumno.php" method="POST" class="form-horizontal">
+		<div class="form-group">
+			<select name="carrera" id="carrera" class="form-control" onchange="getMateria(value)">
+				<option value="0">Seleccione una carrera</option>
+					<?php 
+					if(!empty($carreras)){	
+						foreach ($carreras as $carrera) {
+							echo "<option value='$carrera->codigo'>$carrera->descripcion</option>";
+						}
+					}
+					?>
+			</select>		
+		</div>
+		<div id="Part2"></div>
+		</form>
+	</div>
+<script type="text/javascript" charset="UTF-8" src="../js/jquery.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/scripts.js"></script>
+<script src="../model/alum-ajax.js"></script>
+</body>
+</html>
