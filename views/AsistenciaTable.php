f16bdf53d4ad3cd2c76c6ffc29bbaa5ab59f7b70
valderramagp <valderramagp@gmail.com>
2016-01-27 09:03:26 -0500

    AsistenciaTable
    Agregar parte de la tabla

--- views/AsistenciaTable.php
+++ views/AsistenciaTable.php
@@ -5,5 +5,21 @@
 	include_once("../model/DAOasistencia.php");
 	$db = new DAOasistencia();
 	$assignment = $db->getAssignmentByGrupoAndMateria($grupo, $materia);
+	$daysassignment = $db->getDaysByGrupoAndMateria($grupo, $materia);
+	
 ?>
 <h3>Profesor: <?php echo $assignment[0]->paterno . " " . $assignment[0]->materno . " " . $assignment[0]->nombres;?></h3>
+<div class="table-responsive">
+	<table class="table table-condensed table-striped table-hover">
+		<thead>
+			<tr>
+				<th>Matrícula</th>
+				<th>Nombre</th>
+				<?php foreach ($daysassignment as $column) {
+					echo "<th>$column->dia; </th>";
+				} ?>
+			</tr>
+		</thead>
+	</table>
+</div>
+
