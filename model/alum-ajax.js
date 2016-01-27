9b8970051d0dfb33e4754c70d1488aaf7796aaff
bstsebastian <sebastian.paredes@hotmail.es>
2016-01-27 08:59:00 -0500

    avances grupos y alumnos
    modulo editar alumnos y agregar nuevo grupo

--- model/alum-ajax.js
+++ model/alum-ajax.js
@@ -10,3 +10,29 @@ function getMateria(clave) {
     xhttp.open("GET", "AlumnosFrm2.php?codigo="+clave, true);
     xhttp.send();
 }
+
+function EditAlumnos(matricula) {  
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        var divPanel = document.getElementById('panel');
+        divPanel.innerHTML = "Cargando...";
+        if(xhttp.readyState == 4) {
+            divPanel.innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "../views/AlumnosPanel.php?matricula="+matricula, true);
+    xhttp.send();
+}
+
+function getGrupo(matricula) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        var divGrupos = document.getElementById('Part2');
+        divGrupos.innerHTML = "Cargando...";
+        if(xhttp.readyState == 4) {
+            divGrupos.innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "../views/EditAlumnosFrm2.php?matricula="+matricula, true);
+    xhttp.send();
+}
---
+++ views/AlumnosPanel.php
@@ -0,0 +1,14 @@
+﻿<?php  
+	$id = $_GET['matricula'];
+	include_once("../model/DAOalum.php");
+	$db = new DAOalum();
+
+	
+?>
+
+<div class="clearfix"></div>
+
+<br>
+	<a href="EditAlumnosFrm.php?matricula=<?php echo $id; ?>" class="btn btn-warning" style="width: 40%;">Editar</a>
+	<button class="btn btn-danger pull-right" style="width: 40%;">Eliminar</button>
+	
