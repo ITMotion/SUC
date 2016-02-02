e0f82161e3a579ed205ae46ec0546afaa2c4992b
valderramagp <valderramagp@gmail.com>
2016-02-02 00:07:40 -0500

    Avances de inicio de sesión

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<?php 
+		if (isset($_GET['error'])) {
+			echo "<script> alert('¡Por favor inicia sesión!'); </script>";
+		}
+	?>
 </head>
 <body>
 	<div class="container" id="content">
--- model/login.php
+++ model/login.php
@@ -7,10 +7,14 @@
 	$db->establecerValores($user, $pass);
 	$sesion = $db->comprobarCredenciales();
 	if(!is_null($sesion)) {
+		session_start();
+		$_SESSION['user'] = $user;
 		if ($sesion[0]->tipo == 1) {
+			$_SESSION['permisos']=1;
 			header("Location: ../views/Materias.php");
 		}
 		elseif ($sesion[0]->tipo == 2) {
+			$_SESSION['permisos']=1;
 			header("Location: ../views/Asistencia.php");
 		}
 	}
