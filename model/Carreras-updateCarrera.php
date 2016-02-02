ea3048aa4477b8b2d7735dde07708ca1e61420fa
RockLogin <wayne6@hotmail.com>
2016-02-02 08:56:49 -0500

    edit Carreras

--- model/Carreras-updateCarrera.php
+++ model/Carreras-updateCarrera.php
@@ -1,7 +1,8 @@
 <?php  
 	include_once("DAOcarrera.php");
 	$db = new DAOcarrera();
-	$descripcion = $_POST["carrera"];
-	$db->upCarrera($descripcion);
+	$descripcion = $_POST["descripcion"];
+	$codigo = $_POST["codigo"];
+	$db->upCarrera($codigo, $descripcion);
 	header("Location: ../views/Carreras.php?updsuccess");
 ?>
