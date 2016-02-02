95b741af17ffbc77282d1b5cb58aaac5c9e8edbd
valderramagp <valderramagp@gmail.com>
2016-02-02 09:25:55 -0500

    Carreras cambios

--- views/CarrerasDelete.php
+++ views/CarrerasDelete.php
@@ -3,9 +3,5 @@
 	include_once("../model/DAOcarrera.php");
 	$db = new DAOcarrera();
 	$db->delCarrera($carrera);
-
+	header("Location: Carreras.php?delSuccess");
 ?>
-<div class="alert alert-success col-md-10">
-	<button class="close" data-dismiss="alert"><span>&times;</span></button>
-	Se eliminó correctamente la carrera.
-</div>
